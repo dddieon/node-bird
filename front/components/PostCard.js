2ee0493865caab94eb1930b702004c25dac4b4ae
Jiwon Jeon <ajiop0123@gmail.com>
2021-06-21 03:11:44 +0900

    feature: create post component

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,26 +1,35 @@
-import React, { useState, useCallback } from 'react';
-import { Card, Button, Avatar, Popover, List, Comment } from 'antd';
-import PropTypes from 'prop-types';
-import { RetweetOutlined, HeartTwoTone, HeartOutlined, MessageOutlined, EllipsisOutlined } from '@ant-design/icons';
-import styled from 'styled-components';
-import Link from 'next/link';
+import React, { useState, useCallback } from "react";
+import { Card, Button, Avatar, Popover, List, Comment } from "antd";
+import PropTypes from "prop-types";
+import {
+  RetweetOutlined,
+  HeartTwoTone,
+  HeartOutlined,
+  MessageOutlined,
+  EllipsisOutlined,
+} from "@ant-design/icons";
+import styled from "styled-components";
+import Link from "next/link";
 
-import CommentForm from './CommentForm';
-import PostCardContent from './PostCardContent';
-import PostImages from './PostImages';
-import FollowButton from './FollowButton';
+import CommentForm from "./CommentForm";
+import PostCardContent from "./PostCardContent";
+import PostImages from "./PostImages";
+import FollowButton from "./FollowButton";
 
-const dummyComments = [{
-  User: {
-    nickname: 'nero',
+const dummyComments = [
+  {
+    User: {
+      nickname: "nero",
+    },
+    content: "우와 개정판이 나왔군요~",
   },
-  content: '우와 개정판이 나왔군요~',
-}, {
-  User: {
-    nickname: 'hero',
+  {
+    User: {
+      nickname: "hero",
+    },
+    content: "얼른 사고싶어요~",
   },
-  content: '얼른 사고싶어요~',
-}];
+];
 
 const CardWrapper = styled.div`
   margin-bottom: 20px;
@@ -39,60 +48,61 @@ const PostCard = ({ post }) => {
   }, []);
 
   return (
-    <CardWrapper key={post.id}>
-      <Card
-        cover={post.Images[0] && <PostImages images={post.Images} />}
-        actions={[
-          <RetweetOutlined key="retweet" />,
-          liked
-            ? <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike} />
-            : <HeartOutlined key="heart" onClick={onToggleLike} />,
-          <MessageOutlined key="message" onClick={onToggleComment} />,
-          <Popover
-            key="ellipsis"
-            content={(
-              <Button.Group>
-                <Button>신고</Button>
-                <Button>수정</Button>
-                <Button danger>삭제</Button>
-              </Button.Group>
-            )}
-          >
-            <EllipsisOutlined />
-          </Popover>,
-        ]}
-        extra={<FollowButton post={post} />}
-      >
-        <Card.Meta
-          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
-          title={post.User.nickname}
-          description={<PostCardContent postData={post.content} />}
-        />
-      </Card>
-      {commentFormOpened && (
-        <>
-          <CommentForm post={post} />
-          <List
-            header={`${dummyComments.length} 댓글`}
-            itemLayout="horizontal"
-            dataSource={dummyComments}
-            renderItem={(item) => (
-              <li>
-                <Comment
-                  author={item.User.nickname}
-                  avatar={(
-                    <Link href={{ pathname: '/user', query: { id: item.User.id } }} as={`/user/${item.User.id}`}>
-                      <a><Avatar>{item.User.nickname[0]}</Avatar></a>
-                    </Link>
-                  )}
-                  content={item.content}
-                />
-              </li>
-            )}
-          />
-        </>
-      )}
-    </CardWrapper>
+    <>POST</>
+    // <CardWrapper key={post.id}>
+    //   <Card
+    //     cover={post.Images[0] && <PostImages images={post.Images} />}
+    //     actions={[
+    //       <RetweetOutlined key="retweet" />,
+    //       liked
+    //         ? <HeartTwoTone twoToneColor="#eb2f96" key="heart" onClick={onToggleLike} />
+    //         : <HeartOutlined key="heart" onClick={onToggleLike} />,
+    //       <MessageOutlined key="message" onClick={onToggleComment} />,
+    //       <Popover
+    //         key="ellipsis"
+    //         content={(
+    //           <Button.Group>
+    //             <Button>신고</Button>
+    //             <Button>수정</Button>
+    //             <Button danger>삭제</Button>
+    //           </Button.Group>
+    //         )}
+    //       >
+    //         <EllipsisOutlined />
+    //       </Popover>,
+    //     ]}
+    //     extra={<FollowButton post={post} />}
+    //   >
+    //     <Card.Meta
+    //       avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+    //       title={post.User.nickname}
+    //       description={<PostCardContent postData={post.content} />}
+    //     />
+    //   </Card>
+    //   {commentFormOpened && (
+    //     <>
+    //       <CommentForm post={post} />
+    //       <List
+    //         header={`${dummyComments.length} 댓글`}
+    //         itemLayout="horizontal"
+    //         dataSource={dummyComments}
+    //         renderItem={(item) => (
+    //           <li>
+    //             <Comment
+    //               author={item.User.nickname}
+    //               avatar={(
+    //                 <Link href={{ pathname: '/user', query: { id: item.User.id } }} as={`/user/${item.User.id}`}>
+    //                   <a><Avatar>{item.User.nickname[0]}</Avatar></a>
+    //                 </Link>
+    //               )}
+    //               content={item.content}
+    //             />
+    //           </li>
+    //         )}
+    //       />
+    //     </>
+    //   )}
+    // </CardWrapper>
   );
 };
 
@@ -101,9 +111,11 @@ PostCard.propTypes = {
     id: PropTypes.number,
     User: PropTypes.object,
     content: PropTypes.string,
-    Images: PropTypes.arrayOf(PropTypes.shape({
-      src: PropTypes.string,
-    })),
+    Images: PropTypes.arrayOf(
+      PropTypes.shape({
+        src: PropTypes.string,
+      })
+    ),
     createdAt: PropTypes.object,
   }),
 };
