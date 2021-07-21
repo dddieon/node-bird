13f40e5e2f359b78032b1b406b9df8c2c84527ca
Jiwon Jeon <ajiop0123@gmail.com>
2021-07-22 02:39:18 +0900

    feature: Add Post API (오류나는 중)

--- back/routes/post.js
+++ back/routes/post.js
@@ -1,19 +1,57 @@
 const express = require('express');
 
 const router = express.Router();
+const {Post, Image, Comment, User} = require('../models');
+const {isLoggedIn} = require('./middlewares');
 
-router.get("/", (req,res) => { //POST /get
+router.get("/", (req,res) => {
   res.json([
     { id: 1, content: 'hello' },
     { id: 2, content: 'hello2' }
   ])
 })
 
-router.post("/", (req,res) => { //POST /post
-  res.json([
-    { id: 1, content: 'hello' },
-    { id: 2, content: 'hello2' }
-  ])
+router.post("/", isLoggedIn, async (req,res) => {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      UserId: req.user.id, // ★ passport.js - 디시리얼라이즈 기능
+    })
+    const fullPost = await Post.findOne({ // !! 테이블 조인 !!
+      where: { id: post.id },
+      include: [{
+        model: Image, // !! 게시글에 달린 이미지 !!
+      }, {
+        model: Comment // !! 게시글에 달린 댓글 !!
+      }, {
+        model: User, // !! 게시글 작성자 !!
+      }]
+    })
+    res.status(201).json(post);
+  } catch(error) {
+    console.error(error);
+    next(error);
+  }
+})
+
+router.post(`/:postId/comment`, isLoggedIn, async (req,res) => {
+  try {
+    const isPost = await Post.findOne({
+      where: {id: req.params.postId}
+    })
+    if (!isPost) {
+      return res.status(403).send("존재하지 않는 게시글입니다.");
+    }
+    const comment = await Comment.create({
+      content: req.body.content,
+      PostId: req.params.postId, // ＃ params 로 파라미터 접근
+      UserId: req.user.id, // ★ passport.js - 디시리얼라이즈 기능
+    })
+    res.status(201).json(comment);
+  } catch(error) {
+    console.error(error);
+    next(error);
+  }
 })
 
 router.delete("/", (req,res) => { //DELETE /post
--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,4 +1,4 @@
-import { delay, put, takeEvery, all, fork } from "redux-saga/effects";
+import {delay, put, takeEvery, all, fork, call} from "redux-saga/effects";
 import shortId from "shortid";
 import axios from "axios";
 import {
@@ -88,24 +88,19 @@ function* loadPost(action) {
 }
 
 function addPostAPI(data) {
-  return axios.post("/api/post", data);
+  return axios.post("/post", {content: data});
 }
 
 function* addPost(action) {
   try {
-    yield delay(1000);
-    //const result = yield call(addPostAPI, action.data);
-    const id = shortId.generate();
+    const result = yield call(addPostAPI, action.data);
     yield put({
       type: ADD_POST_SUCCESS,
-      data: {
-        id,
-        content: action.data,
-      },
+      data: result.data,
     });
     yield put({
       type: ADD_POST_TO_ME,
-      data: id,
+      data: result.data.id,
     });
   } catch (e) {
     yield put({
@@ -116,16 +111,15 @@ function* addPost(action) {
 }
 
 function addCommentAPI(data) {
-  return axios.post(`/api/post/${data.postId}/comment`, data);
+  return axios.post(`/post/${data.postId}/comment`, data); // POST /post/1/comment
 }
 
 function* addComment(action) {
   try {
-    yield delay(1000);
-    //const result = yield call(addCommentAPI, action.data);
+    const result = yield call(addCommentAPI, action.data);
     yield put({
       type: ADD_COMMENT_SUCCESS,
-      data: action.data,
+      data: result.data,
     });
   } catch (e) {
     yield put({
