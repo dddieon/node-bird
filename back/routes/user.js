f2da58d5202d7ebccf048850bd6f49bdbe67eb57
Jiwon Jeon <ajiop0123@gmail.com>
2021-07-21 23:33:23 +0900

    fix: User response 수정 (패스워드 제외)

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,23 +1,39 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
-const { User } = require('../models');
+const { User, Post } = require('../models');
+const db = require('../models');
 
 const router = express.Router();
 
 router.post("/login", (req, res, next) => {
   passport.authenticate('local', (err, user, info) => {
-    if (err) { // 에러
+    if (err) { // 1. 에러
       next(err);
     }
-    if (info) { // 요청/응답의 정보
+    if (info) { // 2. 요청/응답의 정보
       return res.status(401).send(info.reason);
     }
     return req.login(user, async(loginErr) => {
-      if (loginErr) { // 패스포트 모듈에서 에러날 때
+      if (loginErr) { // 3. 패스포트 모듈에서 에러날 때
         return next(loginErr);
       }
-      return res.status(200).json(user); // 사용자 정보를 프론트로
+      const fullUserWithoutPassword = await User.findOne({
+        where: {id: user.id},
+        attributes: {
+          exclude: ['password'] // --- 패스워드 컬럼 제외
+        },
+        include: [{
+          model: Post // --- associate 되었던 테이블에서 조인
+        }, {
+          model: User,
+          as: 'Followings' // --- as는 모델정의와 동일하게
+        }, {
+          model: User,
+          as: 'Followers'
+        }]
+      })
+      return res.status(200).json(fullUserWithoutPassword); // 4. 사용자 정보를 프론트로
     })
   })(req, res, next);
 });
