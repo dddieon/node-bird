fe75039ce69536c4e0d8cefa31d2293195936eef
Jiwon Jeon <ajiop0123@gmail.com>
2021-08-04 00:47:42 +0900

    feature: Serverside Render Redux Setting

--- front/pages/index.js
+++ front/pages/index.js
@@ -5,6 +5,9 @@ import PostCard from "../components/PostCard";
 import AppLayout from "../components/AppLayout";
 import { LOAD_POST_REQUEST } from "../reducers/post";
 import { LOAD_MY_INFO_REQUEST } from "../reducers/user";
+import wrapper from "../store/configureStore";
+import { END } from 'redux-saga';
+import axios from 'axios';
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -55,5 +58,22 @@ const Home = () => {
     </AppLayout>
   );
 };
+
+
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req }) => {
+  const cookie = req ? req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  if (req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+  store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+  store.dispatch({
+    type: LOAD_POST_REQUEST,
+  });
+  store.dispatch(END);
+  await store.sagaTask.toPromise(); // saga 요청이 끝날 때 까지 기다림
+});
 
 export default Home;
