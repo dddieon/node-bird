c339a61ac9ec29325f4bd5fdbe5fccfa3b470c6d
Jiwon Jeon <ajiop0123@gmail.com>
2021-08-04 01:57:07 +0900

    feature: Server side render - getServerSideProps(login)

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -5,7 +5,11 @@ import AppLayout from "../components/AppLayout";
 import FollowList from "../components/FollowList";
 import { useDispatch, useSelector } from "react-redux";
 import Router from "next/router";
-import {LOAD_FOLLOWERS_REQUEST, LOAD_FOLLOWINGS_REQUEST} from "../reducers/user";
+import {LOAD_FOLLOWERS_REQUEST, LOAD_FOLLOWINGS_REQUEST, LOAD_MY_INFO_REQUEST} from "../reducers/user";
+import wrapper from "../store/configureStore";
+import axios from "axios";
+import {LOAD_POSTS_REQUEST} from "../reducers/post";
+import {END} from "redux-saga";
 
 const Profile = () => {
   const { me } = useSelector((state) => state.user);
@@ -37,5 +41,21 @@ const Profile = () => {
     </AppLayout>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req }) => {
+  const cookie = req ? req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  if (req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+  store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+  store.dispatch({
+    type: LOAD_POSTS_REQUEST,
+  });
+  store.dispatch(END);
+  await store.sagaTask.toPromise(); // saga 요청이 끝날 때 까지 기다림
+});
 
 export default Profile;
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -4,9 +4,13 @@ import PropTypes from "prop-types";
 
 import AppLayout from "../components/AppLayout";
 import useInput from "../hooks/useInput";
-import { SIGN_UP_REQUEST } from "../reducers/user";
+import {LOAD_MY_INFO_REQUEST, SIGN_UP_REQUEST} from "../reducers/user";
 import { useDispatch, useSelector } from "react-redux";
 import Router from "next/router";
+import wrapper from "../store/configureStore";
+import axios from "axios";
+import {LOAD_POSTS_REQUEST} from "../reducers/post";
+import {END} from "redux-saga";
 
 const TextInput = ({ value }) => <div>{value}</div>;
 
@@ -137,5 +141,21 @@ const Signup = () => {
     </AppLayout>
   );
 };
+
+export const getServerSideProps = wrapper.getServerSideProps((store) => async ({ req }) => {
+  const cookie = req ? req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  if (req && cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+  store.dispatch({
+    type: LOAD_MY_INFO_REQUEST,
+  });
+  store.dispatch({
+    type: LOAD_POSTS_REQUEST,
+  });
+  store.dispatch(END);
+  await store.sagaTask.toPromise(); // saga 요청이 끝날 때 까지 기다림
+});
 
 export default Signup;
