69bc495cdd35e671ca64969a537767fee7092f01
Jiwon Jeon <ajiop0123@gmail.com>
2021-08-06 01:49:44 +0900

    fix: CSS SSR을 위한 document 수정

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -4,6 +4,7 @@ import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
     try {
